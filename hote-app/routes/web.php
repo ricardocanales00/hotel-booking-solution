eb8cb58148d1ac70177887fdfe0903cf9c41d727
Ricardo López Canales <34326913+ricardocanales00@users.noreply.github.com>
2022-11-29 19:59:00 -0600

    Working updates

--- hote-app/routes/web.php
+++ hote-app/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TunelController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
